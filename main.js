b2f1f3d73b3e27a3fa742ae2e502816d77e3ce14
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-12-04 11:19:39 +0100

    modified script to be able to change directory on bigger imgs

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ function changeSlide() {
     if (counter > 3) {
         counter = 1;
     }
-    slider.style.backgroundImage = `url(./images/slider_backgrounds/${counter}.jpg)`;
 
     if (counter === 1) {
         h1.textContent = 'Twój punkt ksero w Koninie';
@@ -16,6 +15,13 @@ function changeSlide() {
         h1.innerHTML = ' <a href="https://www.google.com/maps/dir//Kolska+8,+62-500+Konin/@52.2042904,18.1861445,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x471b316a6f586ecb:0xe16da4fc2ed547c6!2m2!1d18.2561843!2d52.2043128"><i class="fas fa-map-marker-alt"></i> Adres: Kolska 8, Stary Konin</a>';
     } else if (counter === 3) {
         h1.textContent = '20 lat doświadczenia w branży';
+    }
+    if (screen.width < 1198) {
+        slider.style.backgroundImage = `url(./images/slider_backgrounds/${counter}.jpg)`;
+        console.log('working small reso')
+    } else if (screen.width >= 1198) {
+        slider.style.backgroundImage = `url(./images/slider_backgrounds_big_reso/${counter}.jpg)`;
+        console.log('working big reso')
     }
 };
 
